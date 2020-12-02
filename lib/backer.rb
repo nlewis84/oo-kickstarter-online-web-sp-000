7cd1bd27ffddaab3eaad0c4890a1030a8932e6b7
Nathan Lewis <n.lewis.perc@gmail.com>
2020-12-02 04:41:05 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,10 +2,8 @@ class Backer
   attr_accessor :name
   attr_reader :backed_projects
   
-  @backed_projects = []
-
-  def initialize
-    @name = 
+  def initialize(name)
+    @name = name
   end
   
 end
